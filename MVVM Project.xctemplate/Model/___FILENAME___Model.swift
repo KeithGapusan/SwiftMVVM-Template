dd323ba01e12fe31de314322090685f0696fa2a1
Keith Gapusan <keithgapusan@SAMCD-15015s-iMac.local>
2018-07-23 15:43:25 +0800

    initial template of MVVM pattern

---
+++ MVVM Project.xctemplate/Model/___FILENAME___Model.swift
@@ -0,0 +1,14 @@
+//
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Template by Keith Randell Gapusan
+//
+
+
+
+struct ___PROJECTNAME___Model : Decodable {
+    
+}
